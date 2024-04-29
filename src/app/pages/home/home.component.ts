b25e9591d75f712174d36e2693461051de63b120
Dani Arthur Widodo <dani.widodo@krom.id>
2024-04-29 15:52:39 +0700

    add some content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+  contacts = [
+    {
+      name: "Dani Arthur Widodo",
+      tel: "+62 813 417 147",
+      email: "work.daniarthurwidodo@gmail.com",
+    },
+  ];
+  projects = [];
+  skills = [
+    { programmingLanguage: ["dotnet", "javascript", "PHP"] },
+    { frameworks: ["angular", "wordpress"] },
+    { database: ["mongodb", "postgresql"] },
+  ];
+  aboutMe = [""];
+  certificates = [{ name: "", link: "" }];
+  education = [{ name: "", year: "" }];
+  tutorials = [{ name: "", link: "", image: "" }];
+  jobTitle = "";
+  experience = [{ name: "", year: "", jobDescription: "" }];
 }
